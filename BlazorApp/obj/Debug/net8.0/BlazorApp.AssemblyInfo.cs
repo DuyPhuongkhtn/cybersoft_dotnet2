1a1d5ef2355f658711c5f0e612ce0189228e150c
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2025-05-07 21:30:38 +0700

    Session 48: microservice part 1

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a5202281223cbcbbaac520f3df6e38a4e6da9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da4fe9e8293fb0e09ec010760c26c13248fbb304")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
